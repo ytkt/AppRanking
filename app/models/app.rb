75ec07336685c678f8e065218face1e5bca59332
Yuta Kato <yyttkktt@gmail.com>
2013-10-28 12:53:40 +0900

    .find_by_bundle_idと#set_infoを追加

--- app/models/app.rb
+++ app/models/app.rb
@@ -1,2 +1,20 @@
 class App < ActiveRecord::Base
+
+  def self.find_by_bundle_id(bundle_id)
+    app = where(bundle_id: bundle_id).first
+
+    if app.count > 0
+      return app
+    else
+      return create(bundle_id: bundle_id)
+    end
+  end
+
+
+  def set_info(values = {})
+    name = values[:name]
+    developer = values[:developer]
+    img_url = values[:img_url]
+    detail_url = values[:detail_url]
+  end
 end
