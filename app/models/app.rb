cae087448b886623cc27841de7506843299006ea
Yuta Kato <yyttkktt@gmail.com>
2013-10-29 01:18:54 +0900

    スクレイピングメソッド

--- app/models/app.rb
+++ app/models/app.rb
@@ -1,11 +1,28 @@
+require 'open-uri'
+
 class App < ActiveRecord::Base
 
   has_many :rankings
 
+  URL = "https://play.google.com/store/apps/category/COMMUNICATION/collection/topselling_free"
+
+  def self.update_ranking
+    res = open(App::URL, "r", {:ssl_verify_mode=>OpenSSL::SSL::VERIFY_NONE})
+    doc = Nokogiri(res, nil, 'utf-8')
+
+    doc.css('.card.apps.small').each_with_index do |data, index|
+      bundle_id = data['data-docid']
+
+      app = find_by_bundle_id(bundle_id)
+      app.set_info(data) if app.name.nil?
+    end
+  end
+
+
   def self.find_by_bundle_id(bundle_id)
     app = where(bundle_id: bundle_id).first
 
-    if app.count > 0
+    if app
       return app
     else
       return create(bundle_id: bundle_id)
@@ -13,10 +30,12 @@ class App < ActiveRecord::Base
   end
 
 
-  def set_info(values = {})
-    name = values[:name]
-    developer = values[:developer]
-    img_url = values[:img_url]
-    detail_url = values[:detail_url]
+  def set_info(data)
+    self.name       = data.css('.details').css('.title').first['title']
+    self.developer  = data.css('.subtitle').first['title']
+    self.img_url    = data.css('.cover-image').first['src']
+    self.detail_url = data.css('.card-click-target').first['href']
+
+    save
   end
 end
