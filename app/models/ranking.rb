8560d64a96415489233c24540f996953af006cdd
Yuta Kato <yyttkktt@gmail.com>
2013-10-29 23:22:20 +0900

    TOPxを取得するスコープを追加

--- app/models/ranking.rb
+++ app/models/ranking.rb
@@ -2,4 +2,8 @@ class Ranking < ActiveRecord::Base
 
   belongs_to :app
 
+  scope :top, ->(num) {
+    Ranking.includes(:app).where("created_at > ?", Time.now - 1.day).order(:position).limit(num).references(:app)
+  }
+
 end
