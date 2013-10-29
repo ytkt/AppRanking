0fad33181770d067f24547bfb584459e20eef2af
Yuta Kato <yyttkktt@gmail.com>
2013-10-30 01:25:44 +0900

    /をApp#indexに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 AndroRanking::Application.routes.draw do
   get "rankings/index"
   resources :apps
+  root 'apps#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
