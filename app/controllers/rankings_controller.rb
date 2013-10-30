46c611e35ea58bf4a3fa74701f894c46a42c74a5
Yuta Kato <yyttkktt@gmail.com>
2013-10-31 00:11:55 +0900

    Ranking#indexの見た目を編集

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,5 +1,26 @@
 class RankingsController < ApplicationController
   def index
     @rankings = Ranking.top(15)
+
+    @data = []
+    @rankings.each do |rank|
+      @data << rank.app.ranking_last(7).collect { |r| r.position }.reverse
+    end
+    @categories = @rankings.collect { |r| r.created_at.strftime('%m/%d') }.reverse
+
+    @h = LazyHighCharts::HighChart.new("graph") do |f|
+      f.chart(:type => "line")
+      f.title(:text => "Week Ranking")
+      f.xAxis(:categories => @categories)
+      f.yAxis(:max => @data.max+1,
+              :min => 0,
+              :reversed => true,
+              :minTickInterval => 1,
+              :allowDecimals => false,
+              :title => {:text => "rank" })
+      f.series(:name => "rank",
+
+    end
+
   end
 end
