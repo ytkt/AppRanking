509bf90163e1a1e1de4fc48c3cfebd8f761031e3
Yuta Kato <yyttkktt@gmail.com>
2013-11-05 01:54:54 +0900

    FetchDate導入による全体ランキング表示メソッドのコントローラーを修正

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,26 +1,6 @@
 class RankingsController < ApplicationController
   def index
-    @rankings = Ranking.top(15)
-
-    @data = []
-    @rankings.each do |rank|
-      @data << rank.app.ranking_last(7).collect { |r| r.position }.reverse
-    end
-    @categories = @rankings.collect { |r| r.created_at.strftime('%m/%d') }.reverse
-
-    @h = LazyHighCharts::HighChart.new("graph") do |f|
-      f.chart(:type => "line")
-      f.title(:text => "Week Ranking")
-      f.xAxis(:categories => @categories)
-      f.yAxis(:max => @data.max+1,
-              :min => 0,
-              :reversed => true,
-              :minTickInterval => 1,
-              :allowDecimals => false,
-              :title => {:text => "rank" })
-      f.series(:name => "rank",
-
-    end
-
+    @last_fetched = FetchDate.last
+    @rankings = @last_fetched.rankings
   end
 end
