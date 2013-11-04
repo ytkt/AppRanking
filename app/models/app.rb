0a890fdf3b35d46e7b0c54179566224e53a6454e
Yuta Kato <yyttkktt@gmail.com>
2013-11-05 01:41:59 +0900

    ランキングアップデートメソッドをRankingモデルに変更

--- app/models/app.rb
+++ app/models/app.rb
@@ -1,26 +1,6 @@
-require 'open-uri'
-
 class App < ActiveRecord::Base
 
   has_many :rankings
-
-  URL = "https://play.google.com/store/apps/category/COMMUNICATION/collection/topselling_free?hl=jp"
-  PROXY = "http://133.242.137.148:80"
-  PLAY_URL = "https://play.google.com"
-
-  def self.update_ranking
-    res = open(URL, "r", {ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE, proxy: PROXY})
-    doc = Nokogiri(res, nil, 'utf-8')
-
-    doc.css('.card.apps.small').each_with_index do |data, index|
-      bundle_id = data['data-docid']
-
-      app = find_by_bundle_id(bundle_id)
-      app.set_info(data) if app.name.nil?
-
-      app.rankings.create(position: index+1)
-    end
-  end
 
 
   def self.find_by_bundle_id(bundle_id)
--- app/models/ranking.rb
+++ app/models/ranking.rb
@@ -1,4 +1,10 @@
+require 'open-uri'
+
 class Ranking < ActiveRecord::Base
+
+  URL = "https://play.google.com/store/apps/category/COMMUNICATION/collection/topselling_free?hl=jp"
+  PROXY = "http://133.242.137.148:80"
+  PLAY_URL = "https://play.google.com"
 
   belongs_to :app
   belongs_to :fetch_date
@@ -6,5 +12,28 @@ class Ranking < ActiveRecord::Base
   scope :top, ->(num) {
     Ranking.includes(:app).where("created_at > ?", Time.now - 1.day).order(:position).limit(num).references(:app)
   }
+
+
+  def self.update_ranking
+    res = nil
+    begin
+      res = open(URL, "r", {ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE, proxy: PROXY})
+    rescue
+      logger.debug("data fetch failed")
+      return
+    end
+
+    fd = FetchDate.create(fetched_at: Time.now)
+    doc = Nokogiri(res, nil, 'utf-8')
+
+    doc.css('.card.apps.small').each_with_index do |data, index|
+      bundle_id = data['data-docid']
+
+      app = App.find_by_bundle_id(bundle_id)
+      app.set_info(data) if app.name.nil?
+
+      fd.rankings << app.rankings.create(position: index+1)
+    end
+  end
 
 end
