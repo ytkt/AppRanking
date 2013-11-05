cec6f7c25d80a075e7ee9361596b857e49c4db3b
Yuta Kato <yyttkktt@gmail.com>
2013-11-05 09:01:29 +0900

    FetchData基準でDBからデータを引っ張ってくるように変更

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,6 +1,6 @@
 class RankingsController < ApplicationController
   def index
-    @last_fetched = FetchDate.last
-    @rankings = @last_fetched.rankings
+    @last_fetched = FetchDate.last_days(7)
+    @rankings = @last_fetched.first.rankings
   end
 end
