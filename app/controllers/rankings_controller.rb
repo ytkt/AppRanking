cc21eac680c4c396696f8e1f7ed46c2d667137de
Yuta Kato <yyttkktt@gmail.com>
2013-11-06 13:28:01 +0900

    グラフの表示形式を変更

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -13,35 +13,37 @@ class RankingsController < ApplicationController
     @last_fetched.each_with_index do |fd, i|
       fd.rankings.each do |r|
         bundle_id = r.app.bundle_id
+
         unless @data.has_key?(bundle_id)
-          @data[bundle_id] = {}
-          @data[bundle_id]["name"] = r.app.name
-          @data[bundle_id]["positions"] = Array.new(i, nil)
+          @data[bundle_id] = Array.new(i, nil)
         end
 
         # nilで詰める
-        size = @data[bundle_id]["positions"].size
+        size = @data[bundle_id].size
         if size < i
-          @data[bundle_id]["positions"] += Array.new(i-size, nil)
+          @data[bundle_id] += Array.new(i-size, nil)
         end
 
-        @data[bundle_id]["positions"] << r.position
+        @data[bundle_id] << r.position
       end
     end
 
     @h = LazyHighCharts::HighChart.new("graph") do |f|
       f.chart(:type => "line")
+      f.legend(align: "right",
+               layout: "vertical",
+               verticalAlign: 'top')
       f.title(:text => "Week Ranking")
       f.xAxis(:categories => @categories)
-      f.yAxis(:max => 70,
+      f.yAxis(:max => 20,
               :min => 0,
               :reversed => true,
               :minTickInterval => 1,
               :allowDecimals => false,
               :title => {:text => "rank" })
-      @data.each do |key, value|
-        f.series(:name => value["name"],
-                 :data => value["positions"])
+      @data.each do |name, positions|
+        f.series(:name => name,
+                 :data => positions)
       end
     end
   end
